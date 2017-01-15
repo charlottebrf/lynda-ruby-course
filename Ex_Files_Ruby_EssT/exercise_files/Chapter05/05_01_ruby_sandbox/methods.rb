97bfeec16a7f9c074bbd60bf3a3c015f1170a56a
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-15 20:46:44 +0000

    Adds extra notes on definitions

--- Ex_Files_Ruby_EssT/exercise_files/Chapter05/05_01_ruby_sandbox/methods.rb
+++ Ex_Files_Ruby_EssT/exercise_files/Chapter05/05_01_ruby_sandbox/methods.rb
@@ -1,6 +1,8 @@
 def welcome
 	puts "Hello World!"
 end
+#with stand alone methods just call the name
+#can put large blocks of complex code in one method & then call at the same time
 
 def add
 	puts 1 + 1
@@ -21,3 +23,11 @@ def over_five?
 	value = 3
 	puts value > 5 ? 'Over 5' : 'Not over 5'
 end
+
+#define before calling
+
+#object methods .operator - stand alone methods also called functions
+
+#def some_name
+# ...
+#end
