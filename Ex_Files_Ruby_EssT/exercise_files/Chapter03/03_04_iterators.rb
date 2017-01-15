3a139571b42cc07e75cb1866f08c531bede07950
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-15 20:18:33 +0000

    Adds extra comments to iterators exercises

--- Ex_Files_Ruby_EssT/exercise_files/Chapter03/03_04_iterators.rb
+++ Ex_Files_Ruby_EssT/exercise_files/Chapter03/03_04_iterators.rb
@@ -1,8 +1,35 @@
 # This file is a transcript of the IRB session shown in the movie.
-# You should be able to cut and paste it into IRB to get 
+# You should be able to cut and paste it into IRB to get
 # the same results shown in the comments.
 
 # irb --simple-prompt
+#instead of just looping, traverse a fixed set of data- know starting & end point
+#do a code block once for each set of data
+#Iterators use this when you start repeating while loops
+
+#While loop
+#x = 0
+#while x < 5
+# puts "Hello"
+# x += 1
+# end
+
+
+
+#Iterator- reduces the code
+#5.times do
+# puts "Hello"
+#end
+
+#these are the same {} are short hand for a code block
+#1.upto(5) {puts "Hello"}
+#1.downto(1) {puts "Hello"}
+#(1-5).each{puts "Hello"}
+
+#1.upto(5) do |i|
+#  puts "Hello " + num.to_s
+#end
+
 1.upto(5) do |num|
   puts "Hello " + num.to_s
 end
@@ -20,7 +47,7 @@ end
 # Banana
 # Apple
 # Pear
-# => ["banana", "apple", "pear"]
+# => ["banana", "apple", "pear"] -gives the return value of fruits
 for fruit in fruits
   puts fruit.capitalize
 end
@@ -29,3 +56,10 @@ end
 # Pear
 # => ["banana", "apple", "pear"]
 quit
+
+#break
+#next
+#redo
+#retry
+
+#iterators more likely to be used than loops
