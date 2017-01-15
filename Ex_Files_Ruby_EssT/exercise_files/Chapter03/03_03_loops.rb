b01bbf37227c79908eefb756f01e19a6eec347e6
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-15 18:12:10 +0000

    Adds comments to loops exercise

--- Ex_Files_Ruby_EssT/exercise_files/Chapter03/03_03_loops.rb
+++ Ex_Files_Ruby_EssT/exercise_files/Chapter03/03_03_loops.rb
@@ -1,6 +1,15 @@
 # This file is a transcript of the IRB session shown in the movie.
-# You should be able to cut and paste it into IRB to get 
+# You should be able to cut and paste it into IRB to get
 # the same results shown in the comments.
+
+#loop do
+# ...(code block)
+#end
+
+#break
+#next
+#redo
+#retry
 
 # irb --simple-prompt
 x = 0
@@ -37,6 +46,18 @@ end
 # 16
 # 18
 # => nil
+
+#while boolean
+#...
+#end
+
+#until boolean (while something is not true)
+#...
+#end
+
+ # for while & until there is no do included, although it is implied
+
+#matters what is happening at the start of the loop
 x = 0
 # => 0
 while x < 20
@@ -55,3 +76,9 @@ end
 # 20
 # => nil
 quit
+
+#x = 0
+#puts x +=2 while x < 100
+
+#y = 3245
+#puts y /=2 until y <= 1
