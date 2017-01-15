3d0e0e81d00da70d7bc5949c4b1b6f89861df9af
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-15 16:13:20 +0000

    Adds extra comments to symbols exercise:

--- Ex_Files_Ruby_EssT/exercise_files/Chapter02/02_09_symbols.rb
+++ Ex_Files_Ruby_EssT/exercise_files/Chapter02/02_09_symbols.rb
@@ -1,8 +1,10 @@
 # This file is a transcript of the IRB session shown in the movie.
-# You should be able to cut and paste it into IRB to get 
+# You should be able to cut and paste it into IRB to get
 # the same results shown in the comments.
 
 # irb
+#similar to strings & variables
+#symbol stores data 1 time wheras with strings stores it each ime
 :test
 # => :test
 :this_test
@@ -15,7 +17,9 @@
 # => 285760
 :test.object_id
 # => 80818
+#same is reused one time
 hash = {:first_name => 'Kevin', :last_name => 'Skoglund'}
+#hash is variable whereas : is symbol
 # => {:first_name=>"Kevin", :last_name=>"Skoglund"}
 hash['first_name']
 # => nil
@@ -28,4 +32,6 @@ hash[:first_name]
 #        ^
 # 	from (irb):10
 # 	from :0
+
+#if a label used to identify a piece of data or pass a message around, use a symbol 
 quit
