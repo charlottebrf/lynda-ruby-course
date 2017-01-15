a8cb6280596a1418713ef34f3289dfba2611fcac
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-15 14:04:16 +0000

    Adds extra notes on integers exercise

--- Ex_Files_Ruby_EssT/exercise_files/Chapter02/02_03_integers.rb
+++ Ex_Files_Ruby_EssT/exercise_files/Chapter02/02_03_integers.rb
@@ -1,6 +1,8 @@
 # This file is a transcript of the IRB session shown in the movie.
-# You should be able to cut and paste it into IRB to get 
+# You should be able to cut and paste it into IRB to get
 # the same results shown in the comments.
+
+#numbers: integers & floats
 
 # irb
 1 + 1
@@ -19,6 +21,8 @@ x = 4
 # => 4
 x += 2
 # => 6
+#gives result of operation but also changed value of x at the same time
+#Incremented & added assignment
 x
 # => 6
 x = x + 2
@@ -29,6 +33,8 @@ x = x + 2
 # => Fixnum
 123456789123456789.class
 # => Bignum
+#Integers are subclasses- either Fixnum or Bignum
+#Difference is memory value- fixnum is a number more efficient number
 x = 1234 * 1234 * 1234
 # => 1879080904
 x.class
@@ -39,4 +45,5 @@ x.class
 # => 200
 200.next
 # => 201
+#same as 200 + 1
 quit
