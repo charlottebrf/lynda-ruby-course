e554a1abeba6d05669f0eceb414de568f1816316
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-17 07:15:56 +0000

    Adds in revision exercises sheet

---
+++ Ex_Files_Ruby_EssT/revision_exercises.rb
@@ -0,0 +1,32 @@
+# ri #thing_to_search
+
+#math
+4.to_s
+5.0.class
+6.0 / 4
+5 ** 5
+-8.abs
+987.98.round
+987.98.math_floor #math.floor
+987.98.
+
+
+#if /else
+if (x > 0) && (x < 10)
+  puts "Bigger!"
+elsif (x > 0) && (< 20)
+  puts "Biggest"
+else
+  puts "Too big or too small, you choose!"
+end
+
+
+#defining a method
+
+#Arrays
+
+
+#attributes
+
+
+#classes
