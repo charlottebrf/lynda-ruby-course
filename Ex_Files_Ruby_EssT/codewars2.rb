ea9f737fadfba56cc1ed0bbf07a753b0007a7eaf
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-21 20:09:24 +0000

    Adds codewars 2 counting sheep exercises & loops practice

---
+++ Ex_Files_Ruby_EssT/codewars2.rb
@@ -0,0 +1,18 @@
+def countSheeps num_sheep
+  true_count = ""
+  num_sheep.each do |sheep|
+    #if sheep == true
+      true_count = true_count + sheep
+    end
+    return true_count
+  end
+
+
+    #  true_count.each.to_i
+countSheeps([true,false,true,false])
+#num_sheep = [true,  true,  true,  false,
+#          true,  true,  true,  true ,
+#          true,  false, true,  false,
+#          true,  false, false, true ,
+#          true,  true,  true,  true ,
+#          false, false, true,  true ]
---
+++ Ex_Files_Ruby_EssT/loops.rb
@@ -0,0 +1,7 @@
+def looping(num)
+  new_num = 0
+  num.each do |x|
+    new_num = new_num + 1
+end
+
+looping(7)
