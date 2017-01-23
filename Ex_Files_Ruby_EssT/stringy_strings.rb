3ee03834fb46007c592db869d89425b10b44d244
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-23 07:49:15 +0000

    Adds updated version of stringy strings but with broken tests

--- Ex_Files_Ruby_EssT/stringy_strings.rb
+++ Ex_Files_Ruby_EssT/stringy_strings.rb
@@ -1,8 +1,25 @@
 def stringy(size)
-  # Your code here
+  if size > 0 && size.even?
+  "10" * (size / 2)
+  else
+    "Sorry this only works for positive & even numbers "
+  end
 end
 
+stringy(10)
+# RSpec.describe "stringy" do
+#   it "the string always starts with a 1" do
+#      expect(stringy(1)).to eq(1) #i expect this test to fail, giving nil
+#   end
+#   it "given the string's length 2 to the string returns 10" do
+#      expect(stringy(2)).to eq(10) #i expect this test to fail, giving nil
+#   end
+#   it "given the string's length 4 to the string returns 1010" do
+#      expect(stringy(4)).to eq(1010) #i expect this test to fail, giving nil
+#   end
+# end
 
+# 2 => 10,  4 => 1010, 6 => 101010
 
 # Description:
 #
