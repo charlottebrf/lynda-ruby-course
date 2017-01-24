baa5e5bf60e4c4c724c9a4502bcc1f032795aaae
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-24 08:22:56 +0000

    adds in for loop for stringy strings

--- Ex_Files_Ruby_EssT/stringy_strings.rb
+++ Ex_Files_Ruby_EssT/stringy_strings.rb
@@ -1,26 +1,66 @@
-def stringy(size)
-  if size > 0 && size.even?
-  "10" * (size / 2)
-  else
-    "Sorry this only works for positive & even numbers "
-  end
-end
-
-stringy(10)
-# RSpec.describe "stringy" do
-#   it "the string always starts with a 1" do
-#      expect(stringy(1)).to eq(1) #i expect this test to fail, giving nil
-#   end
-#   it "given the string's length 2 to the string returns 10" do
-#      expect(stringy(2)).to eq(10) #i expect this test to fail, giving nil
-#   end
-#   it "given the string's length 4 to the string returns 1010" do
-#      expect(stringy(4)).to eq(1010) #i expect this test to fail, giving nil
+# def stringy(size)
+#   if size == 1
+#     "1"
+#   else
+#     "10" * (size / 2)
 #   end
 # end
 
-# 2 => 10,  4 => 1010, 6 => 101010
+#Always assign vars outside of method
+# def stringy(size)
+#   new_array = (1..size).to_a.map do |number| #method chaining- do block is like an argument to map
+#     if number.odd?
+#       "1"
+#     else
+#       "0"
+#     end
+#   end
+#   new_array.join
+# end
 
+def stringy(size)
+  [1..size].map { |x| x.odd?}.map do |odd|
+    if odd
+      "1"
+    else
+      "0"
+    end
+  end.join
+end
+
+# def stringy(size)
+#   if size.odd?
+#     odd_string(size)
+#   else
+#     even_string(size)
+#   end
+# end
+#
+# def odd_string(size)
+#   "1" + even_string(size-1).reverse
+# end
+#
+# def even_string(size)
+#   "10" * (size / 2)
+# end
+
+RSpec.describe "stringy" do
+ it "the string always starts with a 1" do
+ expect(stringy(1)).to eq("1") #i expect this test to fail, giving nil
+end
+it "given the string's length 2 to the string returns 10" do
+     expect(stringy(2)).to eq("10")
+end
+it "given the string's length 6 to the string returns 101010" do
+     expect(stringy(6)).to eq("101010")
+end
+it "given an odd number it still works" do
+     expect(stringy(3)).to eq("101")
+end
+#   it "given the string's length 4 to the string returns 1010" do
+#      expect(stringy(4)).to eq(1010) #i expect this test to fail, giving nil
+#   end
+end
 # Description:
 #
 # write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
@@ -34,6 +74,20 @@ stringy(10)
 # with size 12 should return : '101010101010'.
 #
 # The size will always be positive and will only use whole numbers.
+
+
+
+# def stringy(size)
+#   if size > 0 && size.even?
+#   "10" * (size / 2)
+#   else
+#     "Sorry this only works for positive & even numbers "
+#   end
+# end
+
+# 2 => 10,  4 => 1010, 6 => 101010
+
+
 
 # Tests:
 # it "Should start with '1'" do
