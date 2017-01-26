219a4fbcc955c3854fed6a32264e55e4b337ba16
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-26 07:16:07 +0000

    Updates with a comment

--- Ex_Files_Ruby_EssT/are_you_playing_the_banjo.rb
+++ Ex_Files_Ruby_EssT/are_you_playing_the_banjo.rb
@@ -1,15 +1,28 @@
 def areYouPlayingBanjo(name)
-  "R"
-  # Implement me!
-  #if your name stars with letter "R" or lower case "r" you are playing the banjo
-  #C# name + "plays banjo" name + "does not play banjo"
-  #Names are always given as valid strings
-  return name
-end
-
-
-RSpec.describe "will print out if you are playing a banjo" do
-  it "checks to see if your name starts with "R"" do
-     expect(areYouPlayingBanjo("R").to eq("R")
+  if name[0] == "R" #|| "r" # why wouldn't || work ? is it because it's too many comparisons 
+    "#{name} plays banjo"
+  elsif name[0] == "r"
+    "#{name} plays banjo"
+  else
+    "#{name} does not play banjo"
   end
 end
+
+areYouPlayingBanjo("Rob")
+
+
+# RSpec.describe "will print out if you are playing a banjo" do
+#   it "checks to see if your name starts with R" do
+#      expect(areYouPlayingBanjo("R").to eq("R")
+#   end
+#   it "checks to see if your name starts with r" do
+#      expect(areYouPlayingBanjo("r").to eq("r")
+#   end
+# end
+#
+# areYouPlayingBanjo("Rob")
+
+# Implement me!
+#if your name stars with letter "R" or lower case "r" you are playing the banjo
+#C# name + "plays banjo" name + "does not play banjo"
+#Names are always given as valid strings
