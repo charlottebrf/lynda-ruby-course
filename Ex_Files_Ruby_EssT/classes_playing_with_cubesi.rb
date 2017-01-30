b313caaf74d05a274de8b2de9ed8f19c8fbeca48
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-30 10:36:47 +0000

    updates with kata link to refer to in review of my solution

--- Ex_Files_Ruby_EssT/classes_playing_with_cubesi.rb
+++ Ex_Files_Ruby_EssT/classes_playing_with_cubesi.rb
@@ -39,6 +39,7 @@ RSpec.describe "Cube" do
   end
 end
 
+#https://www.codewars.com/kata/playing-with-cubes-i/train/ruby
 
 #instructions
 # Create a public class called Cube without a constructor which gets one single private Integer variable Side, a getter GetSide() and a setter SetSide(int num) method for this property. Actually, getter and setter methods are not the common way to write this code in C#. In the next kata of this series, we gonna refactor the code and make it a bit more professional... Note: There's no need to check for negative values!
