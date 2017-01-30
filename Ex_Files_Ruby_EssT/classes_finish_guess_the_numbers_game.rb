4391e0b2867deaf4f4a113a3be1a64d168552904
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-30 09:29:32 +0000

    completes kata on codewars guessing the number game

--- Ex_Files_Ruby_EssT/classes_finish_guess_the_numbers_game.rb
+++ Ex_Files_Ruby_EssT/classes_finish_guess_the_numbers_game.rb
@@ -4,66 +4,50 @@ class Guesser
     @lives = lives
   end
 
-  def guess(n)
-    return false
+  def guess(guess_number)
+    if guess_number == @number
+      true
+    else
+      @lives = @lives - 1 #lives for as long as object guesser
+      if @lives <= 0
+        "run out of lives"
+      else
+        false
+      end
+    end
   end
 end
 
+#classes are usually nouns
+#methods are usually verbs
 
-# Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
+#number == number user has to guess (limited)
+#lives == number of lives the user has left
+#lives == count - the number of lives == the limit
+
+
+# Imagine you are creating a game where the user has to guess the
+
+ #correct number. But there is a limit of how many guesses the user can do.
 #
 # If the user tries to guess more than the limit the function show throw an error
 # If the user guess wrong it should lose a life and return false (if you guess correctly you shouldn't remove a life)
 # If the user guess right it should return true
 # Can you finish the game so all the rules are met?
 
-
-# Test.describe('Basic tests') do
-# Test.it('Correct guess should return true') do
-# guesser = Guesser.new(10, 2)
-# guesser.guess(10)
-# guesser.guess(10)
-# guesser.guess(10)
-# guesser.guess(10)
-# Test.assert_equals(guesser.guess(10), true)
-# end
-#
-# Test.it('Wrong guess should return false') do
-# guesser = Guesser.new(10, 2)
-# guesser.guess(1)
-# Test.assert_equals(guesser.guess(1),false)
-# end
-#
-# Test.it('Lives ran out should throw') do
-# guesser = Guesser.new(10, 2)
-# guesser.guess(1)
-# guesser.guess(2)
-#
-# Test.expect_error('Expect error: "Omae wa mo shindeiru"') do guesser.guess(10) end
-# end
-# end
-#
-# Test.describe('Basic tests') do
-# Test.it('Correct guess should return true') do
-# guesser = Guesser.new(10, 2)
-# guesser.guess(10)
-# guesser.guess(10)
-# guesser.guess(10)
-# guesser.guess(10)
-# Test.assert_equals(guesser.guess(10), true)
-# end
-#
-# Test.it('Wrong guess should return false') do
-# guesser = Guesser.new(10, 2)
-# guesser.guess(1)
-# Test.assert_equals(guesser.guess(1),false)
-# end
-#
-# Test.it('Lives ran out should throw') do
-# guesser = Guesser.new(10, 2)
-# guesser.guess(1)
-# guesser.guess(2)
-#
-# Test.expect_error('Expect error: "Omae wa mo shindeiru"') do guesser.guess(10) end
-# end
-# end
+RSpec.describe "Guesser" do
+  it "returns false when guess number is different to number" do
+    guesser = Guesser.new(8, 2)
+    expect(guesser.guess(5)).to eq (false) #method guess being called on instance guesser
+  end
+  it "returns true when guess number is same as number" do
+    guesser = Guesser.new(8,2)
+    expect(guesser.guess(8)).to eq (true)
+  end
+  it "gives an error message when the user tries to guess more than the limit" do
+    guesser = Guesser.new(8,2)
+    guesser.guess(7) # first attemptt
+    guesser.guess(5) #second attemptt
+    expect(guesser.guess(4)).to eq("run out of lives") #third attempt
+  end
+end
