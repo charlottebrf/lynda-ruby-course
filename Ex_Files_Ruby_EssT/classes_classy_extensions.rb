fd2df43c0cffe1754f9bc5269bf155a42c2f9237
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-30 13:47:55 +0000

    Updates classes extension solution

--- Ex_Files_Ruby_EssT/classes_classy_extensions.rb
+++ Ex_Files_Ruby_EssT/classes_classy_extensions.rb
@@ -1,10 +1,18 @@
 class Cat < Animal
-  #your code here
+
+  def initiatilize(name)
+    @name = name
+  end
+
+  def speak
+    @name + " meows."
+  end
+
 end
 
-RSpec.describe "Cat" do
-  it "" do
-    kitty = Cat.new(some_parameter)
-    expect(kitty.name_of_method(some_parameter)).to eq (some_parameter) #method guess being called on instance guesser
-  end
-end
+# RSpec.describe "Cat" do
+#   it "" do
+#     kitty = Cat.new(some_parameter)
+#     expect(kitty.name_of_method(some_parameter)).to eq (some_parameter) #method guess being called on instance guesser
+#   end
+# end
