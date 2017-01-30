a1abe8a2b6995788415163c617ca52a128ce41a0
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-30 10:35:37 +0000

    Updates classes exercise with solution that passes my tests, but which fail tests of the codewars kata

--- Ex_Files_Ruby_EssT/classes_playing_with_cubesi.rb
+++ Ex_Files_Ruby_EssT/classes_playing_with_cubesi.rb
@@ -1,13 +1,49 @@
-# Code the Cube ^-^
-# Build your Cube without using the initialize function
-# Your cube needs the following:
-	# side = an integer representing the length of the side of the cube
-	# get_side = a function to return side
-	# set_side = a function accepting an int; set side to that int
+class Cube
+
+  def initialize(side = 0,integer) #assign side as a default value of 0 if no value is given
+    @side = side
+    @integer = integer
+  end
+  #Kata prevents me from assigning a side with a value of 0 as it states do not add a constructor. Given this I will resolve the error message given if 0 is given in side through a conditional statement.
+
+  def get_side(side)
+    if side > 0
+      @side
+    else
+      @side = 0
+      return 0
+    end
+  end
+  # side = an integer representing the length of the side of the cube
+  # get_side = a function to return side
+
+  def set_side(integer)
+    @side = @integer
+    return @side
+  end
+  #   set_side = @int
+  #   return @int
+  #   # set_side = a function accepting an int; set side to that int
+end
+
+# set_side = a function accepting an int; set side to that int
+
+RSpec.describe "Cube" do
+  it "should return the length side of the cube " do
+    cube = Cube.new(4,0)
+    expect(cube.get_side(4)).to eq (4)
+  end
+  it "should accept an integer and set the side to that integer" do
+    cube = Cube.new(4,2)
+    expect(cube.set_side(2)).to eq (2)
+  end
+end
 
 
 #instructions
 # Create a public class called Cube without a constructor which gets one single private Integer variable Side, a getter GetSide() and a setter SetSide(int num) method for this property. Actually, getter and setter methods are not the common way to write this code in C#. In the next kata of this series, we gonna refactor the code and make it a bit more professional... Note: There's no need to check for negative values!
+
+
 
 #old tests in kata
 # c = Cube.new
