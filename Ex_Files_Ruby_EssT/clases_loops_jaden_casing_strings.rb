9cb43bedba95dd9ce3d554f841e1c6fff49a1030
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-30 14:59:37 +0000

    updates jaden string kata

--- Ex_Files_Ruby_EssT/clases_loops_jaden_casing_strings.rb
+++ Ex_Files_Ruby_EssT/clases_loops_jaden_casing_strings.rb
@@ -1,13 +1,20 @@
 class String
-  #how to you instiate this class?
-  def toJadenCase(str)
-    new_string = []
-    String.split.each do |word|
-      new_string = word.capitalize
-    end
-      new_string.join.to_s
+
+  def initialize(str)
+    @str = str
   end
-end
+
+  #how to you instiate this class?
+  def to_jaden_case(str)
+    str
+  end
+
+# new_string = []
+#   @str.split.each do |word| #can i do this for a for loop?
+#   new_string = word.capitalize
+# end
+#   new_string.join.to_s
+# end
 
 #Challenge:
 # Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
@@ -18,7 +25,12 @@ end
 #
 # Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 # Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
-
+RSpec.describe "String" do
+  it "should return the string" do
+    string = String.new("codewars is becoming more fun")
+    expect(string.to_jaden_case("codewars is becoming more fun")).to eq ("codewars is becoming more fun")
+  end
+end
 
 # Old Codewars tests
 # str = "How can mirrors be real if our eyes aren't real";
