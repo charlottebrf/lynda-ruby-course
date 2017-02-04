e997ba006d6cf188b228b69c17ff0b66b9e6f247
Charlotte Fereday <cbrfereday@gmail.com>
2017-02-04 12:43:32 +0000

    Updates loop on mumbling  kata

--- Ex_Files_Ruby_EssT/codewars_mumbling.rb
+++ Ex_Files_Ruby_EssT/codewars_mumbling.rb
@@ -12,11 +12,24 @@
 #then adds that duplicate characater as a new string
 
 def mumbling(string)
-    new_string = string.split("").map do |letter|
-      letter + letter
+    result_array = []
+    string.split("").each_with_index do |letter, index|
+      result_array.push(letter * (index + 1))#need to +1 due to index starting at 0
    end
-   new_string.join
+   p result_array.join("-")
 end
 
+#.push adds to an array like + ""
+#.capitzalize needs to be added in as separate paranthesis 
 
-p mumbling("Abc")
+RSpec.describe "mumbling" do
+  # it "given a string should return return the characters doubled in the string" do
+  #   expect(mumbling("abc")).to eq ("aabbcc")
+  # end
+  # it "given a string should return return the character multiplied by its index" do
+  #   expect(mumbling("abc")).to eq ("abbccc")
+  # end
+  it "given a string should return return the character multiplied by its index with a hyphen inbetween each character " do
+    expect(mumbling("abc")).to eq ("a-bb-ccc")
+  end
+end
