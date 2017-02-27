32c260f9c0d4409e64810d572dafca1a0790ed6b
Charlotte Fereday <cbrfereday@gmail.com>
2017-02-27 23:10:10 +0000

    Updates solution to exercise

--- Kata/welcome_to_the_city.rb
+++ Kata/welcome_to_the_city.rb
@@ -1,9 +1,9 @@
 def say_hello(name, city, state)
-   name = name[0] << name[1]
-   name = name.split(" ") #need to correctly split the string 
-  "Hello #{name}! Welcome to #{city}, #{state}!"
+   name = name.join(" ")
+  p "Hello #{name}! Welcome to #{city}, #{state}!"
 end
 
+say_hello(["John", "Smith"], "Gdansk", "anywhere")
 
 # Create a method say_hello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space betweeen each, and the length of the name array in test cases will vary.
 #
